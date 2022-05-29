44c0775b3cb854e60c57526f0370ba474233d0f4
YYYYYM <1669850480@qq.com>
2022-05-29 21:23:25 +0800

    hotfix重新第一次提交

--- src/main/java/com/YYYYYM/git/gitTest.java
+++ src/main/java/com/YYYYYM/git/gitTest.java
@@ -3,16 +3,6 @@ package com.YYYYYM.git;
 public class gitTest {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello gitHub!");
-        System.out.println("Hello gitHub!");
-        System.out.println("Hello gitHub!");
-        System.out.println("Hello gitHub!");
-        System.out.println("Hello gitHub!");
-        System.out.println("Hello gitHub!");
-        System.out.println("Hello gitHub!");
-        System.out.println("Hello gitHub!");
-        System.out.println("测试测试");
         System.out.println("hot-fix分支添加内容");
     }
 
