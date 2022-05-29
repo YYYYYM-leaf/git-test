cc5ab6c4db4688b2f5bc7c45dc5677bc4c8c01b6
YYYYYM <1669850480@qq.com>
2022-05-29 20:54:57 +0800

    secind commit

--- src/main/java/com/YYYYYM/git/gitTest01.java
+++ src/main/java/com/YYYYYM/git/gitTest01.java
@@ -1,7 +1,10 @@
 package com.YYYYYM.git;
 
+import java.util.Arrays;
+
 public class gitTest01 {
     public static void main(String[] args) {
         System.out.println("gitTest01.main");
+        System.out.println("args = " + Arrays.deepToString(args));
     }
 }
