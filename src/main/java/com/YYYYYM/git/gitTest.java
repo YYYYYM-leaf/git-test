74db90269b0bd1bdbf0ba60b73571490fe20bd4f
YYYYYM <1669850480@qq.com>
2022-05-29 21:20:41 +0800

    master重新第一次提交

--- src/main/java/com/YYYYYM/git/gitTest.java
+++ src/main/java/com/YYYYYM/git/gitTest.java
@@ -4,16 +4,6 @@ public class gitTest {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello gitHub!");
-        System.out.println("Hello gitHub!");
-        System.out.println("Hello gitHub!");
-        System.out.println("Hello gitHub!");
-        System.out.println("Hello gitHub!");
-        System.out.println("Hello gitHub!");
-        System.out.println("Hello gitHub!");
-        System.out.println("Hello gitHub!");
-        System.out.println("测试测试");
-        System.out.println("hot-fix分支添加内容");
     }
 
 }
