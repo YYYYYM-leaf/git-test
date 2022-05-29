604d3fd8454ca6b2e0825ed18401b2d4ab9294bd
YYYYYM <1669850480@qq.com>
2022-05-29 20:52:03 +0800

    frist commit

---
+++ src/main/java/com/YYYYYM/git/gitTest.java
@@ -0,0 +1,10 @@
+package com.YYYYYM.git;
+
+public class gitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+    }
+
+}
---
+++ src/main/java/com/YYYYYM/git/gitTest01.java
@@ -0,0 +1,7 @@
+package com.YYYYYM.git;
+
+public class gitTest01 {
+    public static void main(String[] args) {
+        System.out.println("gitTest01.main");
+    }
+}
